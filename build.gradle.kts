02b2170bf6dd5b45c460ed4d2e361a5bc5c37986
Kevin Zou <43654611+KevinnZou@users.noreply.github.com>
2023-10-18 15:46:43 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    kotlin("multiplatform").apply(false)
+    id("com.android.application").apply(false)
+    id("com.android.library").apply(false)
+    id("org.jetbrains.compose").apply(false)
+    id("com.vanniktech.maven.publish") version "0.25.3" apply false
+    id("org.jetbrains.dokka")
+}
